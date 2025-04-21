1cf9e7d860df3dad2848be14ff5ff87c462520b4
JeromeF91 <foufoudu91@gmail.com>
2025-04-21 17:56:12 -0400

    biint for amount

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -4,7 +4,7 @@ export const buyevent = onchainTable("buyFromListings", (t) => ({
   id: t.text().primaryKey(),
   tokenAddress: t.text().notNull(),
   to: t.text().notNull(),
-  amount: t.integer().notNull(),
+  amount: t.bigint().notNull(),
 }));
 
 
